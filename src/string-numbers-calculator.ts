7a6d6b925e134a97360609ad10c59c833575743a
Sagar Gopale <sagar090989@gmail.com>
2025-02-16 23:48:16 +0530

    Updated add function to include delimiter change input and refactored code for early detection of non numeric values

--- src/string-numbers-calculator.ts
+++ src/string-numbers-calculator.ts
@@ -6,13 +6,19 @@ export class StringNumberCalculator {
 		}
 		let delimiterPattern = /[\n,]+/;
 
-		const inputNumbers = input.split(delimiterPattern).map(Number);
+		const changeDelimiterMatch = input.match(/^\/\/([^\n]+)\n/);
+    if (changeDelimiterMatch) {
+      delimiterPattern = new RegExp(`[${changeDelimiterMatch[1]}\n]+`);
+      input = input.replace(changeDelimiterMatch[0], '');
+    }
+
+		const inputNumbers = input.split(delimiterPattern).map(inputNumber => {
+			if(Number.isNaN(Number(inputNumber))) throw new Error('Invalid string: Non-numeric values detected!');
+			return Number(inputNumber);
+		});
 
 		for(let i = 0; i < inputNumbers.length; i++) {
 			sum = sum + inputNumbers[i];
-		}
-		if(Number.isNaN(sum)) {
-			throw new Error('Invalid string: Non-numeric values detected!');
 		}
 		return sum;
 	}
